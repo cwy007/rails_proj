f7d4851e88f157eed8ac3d1b1127737a0adf69ab
chenweiyan <chanweiyan007@gmail.com>
2022-06-28 10:25:22 +0800

    docs: 配置跨域

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,13 @@ module RailsProj
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # https://blog.csdn.net/topswim/article/details/79311592
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
