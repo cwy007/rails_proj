67a5b4b8d50889f9f6cef76466ef77aa5ee97a1e
chenweiyan <chanweiyan007@gmail.com>
2022-06-28 15:53:09 +0800

    fix: update redis host ip

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 # https://www.jianshu.com/p/add3471b2328?utm_campaign=maleskine&utm_content=note&utm_medium=seo_notes&utm_source=recommendation
 REDIS_CONFIG = {
-  host: Rails.env.production? ? '8.131.71.102' : 'localhost',
+  host: '127.0.0.1',
   port: 6379,
   # db: Rails.env.test? ? 3 : 0
 }
