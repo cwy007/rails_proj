5eb4e8e6be6a9088b20998ec5a047a007e62decd
chenweiyan <chanweiyan007@gmail.com>
2023-05-31 17:32:35 +0800

    fix: 解决返回的重量是科学计数法的格式 1.0e-06 -> 0.000001，使用普通的小数返回

--- app/controllers/api/scales_controller.rb
+++ app/controllers/api/scales_controller.rb
@@ -13,13 +13,14 @@ class Api::ScalesController < ApplicationController
 
   def weight
     $tmp_redis.set('weight', params[:body] && params[:body][:weight]) if params[:body] && params[:body][:weight];
+    weight = ("%.15f" % $tmp_redis.get('weight')).sub(/0*$/,"");
     render json: {
       code: 200,
       message: '成功',
       data: {
         list: [{
           scale: params[:body] && params[:body][:scales] && params[:body][:scales][0] || 'ABC123',
-          weight: ($tmp_redis.get('weight') || rand(1..20)).to_f,
+          weight: weight || rand(1..20),
           success: 1,
           unit: 'kg',
           msg: 'weight 1-20',
